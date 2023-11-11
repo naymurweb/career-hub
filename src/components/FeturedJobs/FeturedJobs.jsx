121fe2e1af7de297c1552d3b0f02a78acfd0eade
naymur <naymurweb@gmail.com>
2023-11-11 22:12:33 +0600

    see all jobs btn add

--- src/components/FeturedJobs/FeturedJobs.jsx
+++ src/components/FeturedJobs/FeturedJobs.jsx
@@ -1,8 +1,10 @@
-import  { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Job from "../Job/Job";
 
 const FeturedJobs = () => {
   const [jobs, setJobs] = useState([]);
+  const [dataLength, setDataLength] = useState(4);
+
   useEffect(() => {
     fetch("jobs.json")
       .then((res) => res.json())
@@ -12,12 +14,25 @@ const FeturedJobs = () => {
     <div className="">
       <div className="text-center space-y-4">
         <h2 className="text-5xl font-bold">Featured Jobs</h2>
-        <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
+        <p>
+          Explore thousands of job opportunities with all the information you
+          need. Its your future
+        </p>
       </div>
       <div className="grid md:grid-cols-2 mt-8 gap-6">
-      {
-        jobs.map(job=><Job key={job.id} job={job}></Job>)
-      }
+        {jobs.slice(0, dataLength).map((job) => (
+          <Job key={job.id} job={job}></Job>
+        ))}
+      </div>
+      <div className="text-center my-5">
+        <button
+          className={`btn btn-primary ${
+            dataLength === jobs.length && "hidden"
+          }`}
+          onClick={() => setDataLength(jobs.length)}
+        >
+          See All Jobs
+        </button>
       </div>
     </div>
   );
